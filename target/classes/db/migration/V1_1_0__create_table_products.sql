c87388b5d8080825b50728320a928f66e7d77ef2
Valinstein <valinstein@ukr.net>
2021-08-19 22:08:50 +0300

    removed service folders

--- target/classes/db/migration/V1_1_0__create_table_products.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    product_name VARCHAR(255),
-    product_price REAL,
-    creation_date timestamp);
--- target/classes/db/migration/V1_1_1__add_test_products.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO products(product_name, product_price, creation_date) VALUES ('lamp', 200.00, '2021-08-19 10:23:54');
-INSERT INTO products(product_name, product_price, creation_date) VALUES ('chair', 150.50, '2021-08-17 11:13:44');
